29e83f0dfef95511833c5258b6cf12a88a566513
Shadow04466 <124391371+Shadow04466@users.noreply.github.com>
2025-12-23 23:56:26 +0500

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,1 +1,41 @@
 
+import { auth } from "./firebase.js";
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+const msg = document.getElementById("msg");
+
+if (loginBtn) {
+  loginBtn.addEventListener("click", async () => {
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!email || !password) {
+      msg.innerText = "Email and password required";
+      return;
+    }
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = "/admin/dashboard.html";
+    } catch (err) {
+      msg.innerText = err.message;
+    }
+  });
+}
+
+onAuthStateChanged(auth, user => {
+  const path = window.location.pathname;
+  if (!user && path.includes("/admin/dashboard.html")) {
+    window.location.href = "/admin/login.html";
+  }
+});
+
+window.logout = async () => {
+  await signOut(auth);
+  window.location.href = "/admin/login.html";
+};
