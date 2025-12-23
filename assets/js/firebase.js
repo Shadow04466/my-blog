d334982a99d1c5849cf17ac9171dabcbdbfd5149
Shadow04466 <124391371+Shadow04466@users.noreply.github.com>
2025-12-23 23:45:53 +0500

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,9 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAc3G14YFLUtEfRZWjmGBU4OEzOrbK77fM",
+  apiKey: "YOUR_API_KEY",
   authDomain: "my-earning-blog.firebaseapp.com",
   projectId: "my-earning-blog",
   storageBucket: "my-earning-blog.appspot.com",
@@ -12,5 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
 export const db = getFirestore(app);
