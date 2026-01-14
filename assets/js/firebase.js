b5d4678bdb50bb3925e39c7e95904f261e55cfbe
Shadow04466 <124391371+Shadow04466@users.noreply.github.com>
2026-01-14 22:35:07 +0500

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,17 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
+  apiKey: "AIzaSyAc3G14YFLUtEfRZWjmGBU4OEzOrbK77fM",
   authDomain: "my-earning-blog.firebaseapp.com",
   projectId: "my-earning-blog",
-  storageBucket: "my-earning-blog.appspot.com",
+  storageBucket: "my-earning-blog.firebasestorage.app",
   messagingSenderId: "591454593500",
-  appId: "1:591454593500:web:fac21a06e5faa542f1af19"
+  appId: "1:591454593500:web:fac21a06e5faa542f1af19",
+  measurementId: "G-XG6V1Z5TVK"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
